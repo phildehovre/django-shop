6124bc8f14f5b782df11d2e855c13acb45b4b251
Philippe De Hovre <ph.dehovre@gmail.com>
2024-04-25 09:57:45 +0100

    Merge todo4

--- base/urls.py
+++ base/urls.py
@@ -8,6 +8,7 @@ from . import views
 urlpatterns = [
     path('', views.home_view, name="home"),
     path('about/', views.about_view, name="about"),
+    path('account/', views.account_view, name="account"),
     path('login/', views.login_view, name='login_register'),
     path('logout/', views.user_logout, name='user_logout'),
     path('register/', views.user_register, name='user_register'),

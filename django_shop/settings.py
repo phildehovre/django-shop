6ef3fb9574d5f0765aeb984a0454bf7bba4e4ad8
philippedehovre <ph.dehovre@gmail.com>
2024-05-20 13:30:41 +0100

    Add ALLOWED_HOSTS setting

--- django_shop/settings.py
+++ django_shop/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-(f#$f9fnm*deg4+q510p5axz^d)24ws0po3exm6gft&1a%#)wz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app', '*']
 
 
 # Application definition
